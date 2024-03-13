74067d7480847b416c62491524d6e427edddf834
jasurxabibullayev029 <xabibullayev186@gmail.com>
2024-03-13 17:09:31 +0500

    I want to learn so I add this commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// let text = "jasur"
+// let uzunligi = text. length;
+
+// alert(uzunligi)
+
+let name = "Jasur"
+let test = name.includes("a")
+
+if (test) {
+    alert("Bu hharif mavjud ")
+}else{
+    alert("Bu hharif mavjud emas")
+}
+alert(test)
